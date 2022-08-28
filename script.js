8c383656e3ddaf952237751b1deaa65f700235e8
DrMcGoodTimes <103394997+DrMcGoodTimes@users.noreply.github.com>
2022-08-28 03:56:11 -0400

    Clicking newColorbutton makes a new color

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const NEWCOLORBUTTON = document.getElementById('newColorButton');
+const HEXARRAY = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"]
 
+NEWCOLORBUTTON.addEventListener('click', function() {
+    let randomColor = "";
+
+    for (let i = 0; i<6; i++) {
+        randomColor += HEXARRAY[Math.floor(Math.random()*HEXARRAY.length)];
+    }
+
+    document.body.style.backgroundColor = `#${randomColor}`;
+});
